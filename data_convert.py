56fa4956028fcdc87417c4b2849394daa14f60e4
蝦米碗糕 <aaa12345.59487@gmail.com>
2024-03-04 20:08:28 +0800

    Changed something

--- data_convert.py
+++ data_convert.py
@@ -10,18 +10,25 @@ daily_avg = pd.DataFrame()
 
 empty = []
 for file in FILE_LIST:
+    # Extract date from file name
+    date_str = "".join(file.stem.split("-")[1:])
+    file_date = pd.to_datetime(date_str, format="%Y%m%d")
+
     # Read csv file
     try:
         df = pd.read_csv(file, skiprows=1, na_values=['--', '\\', '/', '&', 'X'])   # fucking stupid
-    except pd.errors.EmptyDataError:
+    except pd.errors.EmptyDataError as e:
         empty.append(file)
         continue
 
-    avg_values = df[["StnPres","Temperature","RH","WS","WD","Precp"]].mean()
+    df['Date'] = file_date
 
-    daily_avg = pd.concat([daily_avg, avg_values], axis=1)
+    df = df[['Date',"StnPres","Temperature","RH","WS","WD","Precp"]]
 
-daily_avg = daily_avg.transpose()
+    daily_avg = pd.concat([daily_avg, df])
+
+
+daily_avg = daily_avg.groupby('Date').mean()
 
 OUTPUT_FOLDER = Path("./data")
 OUTPUT_NAME = Path("daily_average.csv")
@@ -31,6 +38,6 @@ OUTPUT_PATH = OUTPUT_FOLDER / OUTPUT_NAME
 if not OUTPUT_FOLDER.is_dir():
     OUTPUT_FOLDER.mkdir(parents=True, exist_ok=True)
 
-daily_avg.to_csv(OUTPUT_PATH, index=False)
+daily_avg.to_csv(OUTPUT_PATH, index=True)
 
 print(f"Average data saved to {OUTPUT_PATH}")
